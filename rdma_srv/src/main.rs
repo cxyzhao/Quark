b2b66a357a24fde67e36d56535a1f06f4e6c3491
cxyzhao <cxyzhao@cs.washington.edu>
2025-12-22 10:21:39 +0000

    working version for multiple tenants

--- rdma_srv/src/main.rs
+++ rdma_srv/src/main.rs
@@ -308,9 +308,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // }
 
     unsafe {
+        let quark_sgiov_container_id = std::env::var("QUARK_SGIOV_CONTAINER_ID")
+            .unwrap_or_else(|_| "0".to_string())
+            .parse::<u16>();
+
         let serv_addr: libc::sockaddr_in = libc::sockaddr_in {
             sin_family: libc::AF_INET as u16,
-            sin_port: 8888u16.to_be(),
+            sin_port: (8888u16 + quark_sgiov_container_id.unwrap_or(0)).to_be(),
             sin_addr: libc::in_addr {
                 s_addr: u32::from_be_bytes([0, 0, 0, 0]).to_be(),
             },
@@ -453,9 +457,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         #[cfg(not(offload = "yes"))]{
             udp_s_addr = u32::from_be_bytes([192, 168, 2, 7]).to_be();
         }
+
+                
+        let quark_sgiov_container_id = std::env::var("QUARK_SGIOV_CONTAINER_ID")
+        .unwrap_or_else(|_| "0".to_string())
+        .parse::<u16>();
+
         let srv_udp_addr: libc::sockaddr_in = libc::sockaddr_in {
             sin_family: libc::AF_INET as u16,
-            sin_port: 3340u16.to_be(),
+            sin_port: (3340u16 + quark_sgiov_container_id.unwrap_or(0)).to_be(),
             sin_addr: libc::in_addr {
                 s_addr: udp_s_addr,
             },
@@ -1063,9 +1073,13 @@ fn SetupConnection(ip: &u32) {
 
     RDMA_SRV.conns.lock().insert(node.ipAddr, rdmaConn.clone());
     unsafe {
+        let quark_sgiov_container_id = std::env::var("QUARK_SGIOV_CONTAINER_ID")
+        .unwrap_or_else(|_| "0".to_string())
+        .parse::<u16>();
+
         let serv_addr: libc::sockaddr_in = libc::sockaddr_in {
             sin_family: libc::AF_INET as u16,
-            sin_port: 8888u16.to_be(), //8888 is the port for RDMASvc to shake hands
+            sin_port: (8888u16 + quark_sgiov_container_id.unwrap_or(0)).to_be(), //8888 + QUARK_SGIOV_CONTAINER_ID is the port for RDMASvc to shake hands
             sin_addr: libc::in_addr {
                 s_addr: node.ipAddr,
             },
@@ -1086,7 +1100,12 @@ fn gen_eventfd() -> RawFd {
 
     let efd = unsafe { libc::eventfd(0, 0) };
 
-    let client_sendfd_sock_fd = UnixSocket::NewClient("/EVENTFDSOCKET").unwrap();
+    let quark_sgiov_container_id = std::env::var("QUARK_SGIOV_CONTAINER_ID")
+    .unwrap_or_else(|_| "0".to_string())
+    .parse::<u16>();
+    let client_sendfd_sock_path = format!("/EVENTFDSOCKET{}", quark_sgiov_container_id.unwrap_or(0));
+    let client_sendfd_sock_fd = UnixSocket::NewClient(&client_sendfd_sock_path).unwrap();
+    
     let client_sendfd_sock = UnixSocket { fd: client_sendfd_sock_fd };
     let res = client_sendfd_sock.SendFd(efd.as_raw_fd());
     drop(client_sendfd_sock);
